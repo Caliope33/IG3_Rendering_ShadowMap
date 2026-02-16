64410c2834195cbaa709c9fc686c44fce1c0cf10
Alexandra Villon <a.villon.h@gmail.com>
2026-02-16 18:44:34 +0100

    InputHandler FILES

---
+++ src/rendu/materials/InputHandler.h
@@ -0,0 +1,11 @@
+#ifndef INPUT_HANDLER_H
+#define INPUT_HANDLER_H
+
+#include <GLFW/glfw3.h>
+
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+#endif
